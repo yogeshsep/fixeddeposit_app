14f59982fb9103f9c8778b508763bdf8623e4cd0
yogeshsep <yogeshsep15@gmail.com>
2014-06-16 10:10:52 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FixeddepositApp::Application.config.secret_token = 'a376e5eaf333cd4d4dd05609b4dbb686ab9e764738159a17907acbcb6e536ccd87e221bb3ddbd987cfa16b5667f3379b027865c010465df1ddb966677693c746'
